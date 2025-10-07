56c01870809391d7a32c1812bff10da60a0a8192
eloahsilva <eloah2412@gmail.com>
2025-10-07 07:28:21 -0300

    Configuração inicial do Tailwind

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarmenu() {
-    let menu_mobile = document.querySelector('.burger');   
-    if (menu_mobile.classList.contains('open')) {
-        menu_mobile.classList.remove('open');
-    } else {
-        menu_mobile.classList.add('open');
-    }
-}
